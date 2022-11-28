dbbe508ead6c8f173dc8ff903303c6a670c804be
Patryk <patrykokpara@hotmail.com>
2022-11-28 00:02:43 +0000

    connected to mongodb

--- fitlog/index.js
+++ fitlog/index.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true }
+);  
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.listen(3200, () => {
     console.log("We are going on port 3200")
-})
+}) 
