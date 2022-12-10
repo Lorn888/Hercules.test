27738bb905628219adc1f12641bcb70b9f9fddaa
Patryk <patrykokpara@hotmail.com>
2022-12-10 20:56:26 +0000

    Added "new workout" functionality

--- fitlog/index.js
+++ fitlog/index.js
@@ -5,6 +5,7 @@ const Workout = require('./models/workout')
 
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+const methodOverride = require("method-override")
 
 require('dotenv').config();
 const uri = process.env.ATLAS_URI;
@@ -17,13 +18,35 @@ connection.once('open', () => {
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
+
+
 
 app.get('/', async (req, res) => {
     const workouts = await Workout.find({})
     console.log(workouts)
-    res.render("home")
+    res.render("home", {workouts})
 })
- 
+
+app.get("/workout/:id", async (req, res) => {
+    const { id } = req.params;
+    const workouts = await Workout.findById(id)
+    res.render("detail", {workouts})
+})
+
+app.get("/new", async (req, res) => {
+    res.render("new")
+})
+
+app.post("/workouts/", async (req, res) => {
+    const newWorkout = new Workout(req.body);
+    await newWorkout.save()
+    res.redirect(`workout/${newWorkout._id}`)
+})
+
+
+    
 app.listen(3200, () => {
     console.log("We are going on port 3200")
 }) 
